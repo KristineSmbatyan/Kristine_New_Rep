9567dc1db447add91887b0106888f4523458a077
KristineSmbatyan <81805696+KristineSmbatyan@users.noreply.github.com>
2021-05-12 13:01:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,41 @@ app.get("/dashboard", (req, res) => {
 app.get("/login", (req, res) => {
 	res.render("login");
 })
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+app.get('/profile', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('profile', {user});
+})
+
+app.get('/protectedRoute', checkAuthenticated, (req,res)=>{
+    res.send('This route is protected')
+})
+
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
+
+})
 app.listen(PORT, ()=>{
 	console.log("Server Started on: "+ PORT);
 })
